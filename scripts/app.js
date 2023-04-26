59cc10e5b999211737d0d552c2659f19fbe4c632
ElaineBergstrom <elainebergstrom26@gmail.com>
2023-04-26 18:42:51 +0200

    create button element for books

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 const modals = document.querySelectorAll("[data-modal]");
+let bookNookLibrary = [];
 
 modals.forEach(function (trigger) {
   trigger.addEventListener("click", function (event) {
@@ -15,13 +16,40 @@ modals.forEach(function (trigger) {
   });
 });
 
-const testBook = [
-  {
-    id: 1,
-    title: "Matilda",
-    author: "Roald Dahl",
-    pages: 239,
-    published: "October 1, 1988",
-    read: true,
-  },
-];
+
+// Constructor
+function Book(title, author, pages, published, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.published = published;
+  this.read = read;
+}
+// test book object
+const newBook = new Book(
+  "Matilda",
+  "Roald Dahl",
+  239,
+  "october 1, 1988",
+  "yes"
+);
+
+console.log(newBook);
+
+//Create button element for book in hmtl
+const bookButton = document.createElement("button");
+bookButton.classList.add("book");
+
+//Create title element based off books title
+const bookTitle = document.createElement("p");
+bookTitle.textContent = `"${newBook.title}"`;
+bookTitle.classList.add("book-title");
+//Assigns bookTitle as Child to bookButton
+bookButton.appendChild(bookTitle);
+
+
+
+const bookContainer = document.querySelector(".shelf");
+bookContainer.appendChild(bookButton);
+
+function addBookToLibrary() {}

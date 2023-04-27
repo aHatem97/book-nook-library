cb8e6ae6356fbbb52fd9f6b641ecc8231f3629df
aHatem97 <ahmedhatem96@hotmail.co.uk>
2023-04-27 10:46:14 +0200

    added all modals & add new book functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,7 @@
 const modals = document.querySelectorAll("[data-modal]");
+const addForm = document.querySelector(".modal-add-form");
+const delForm = document.querySelector(".modal-delete-form");
+const bookContainer = document.querySelector(".shelf");
 
 modals.forEach(function (trigger) {
   trigger.addEventListener("click", function (event) {
@@ -23,50 +26,57 @@ function Book(title, author, pages, published, read) {
   this.published = published;
   this.read = read;
 }
-// Uses dummy info right now, but we can leave it as an empty []
-let bookNookLibrary = [
-  {
-    title: "Matilda",
-    author: "Roald Dahl",
-    pages: 239,
-    published: "October 1, 1988",
-    read: "yes",
-  },
-  {
-    title: "A Game of Thrones",
-    author: "George R.R Martin",
-    pages: 694,
-    published: "August 1, 1996",
-    read: "yes",
-  },
-  {
-    title: "The Hobbit",
-    author: "J.R.R Tolkien",
-    pages: 310,
-    published: "September 21, 1937",
-    read: "yes",
-  },
-];
 
-const bookContainer = document.querySelector(".shelf");
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-bookNookLibrary.forEach(function (book) {
-  //Create button element for book in hmtl
-  const bookButton = document.createElement("button");
-  bookButton.classList.add("book");
-  //creates pop up... but can't exit it without refreshing page
-  bookButton.addEventListener("click", function () {
-    console.log("Book details");
+  const addBook = new Book(
+    document.getElementById("book-title").value,
+    document.getElementById("author").value,
+    document.getElementById("pages-count").value,
+    document.getElementById("publish-date").value,
+    document.getElementById("read-book").value
+  );
+
+  let allBooks = [addBook];
+
+  allBooks.forEach(function (book) {
+    //Create button element for book in hmtl
+    const bookButton = document.createElement("button");
+    bookButton.classList.add("book");
+    bookButton.setAttribute("data-modal", "modal-book");
+    //creates pop up... but can't exit it without refreshing page
+    bookButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      const modal = document.getElementById(bookButton.dataset.modal);
+      modal.classList.add("open");
+      const exits = modal.querySelectorAll(".modal-exit");
+      exits.forEach(function (exit) {
+        exit.addEventListener("click", function (event) {
+          event.preventDefault();
+          modal.classList.remove("open");
+        });
+      });
+      //populating modal with our object
+      const outputTitle = (document.getElementById("output-title").value =
+        book.title);
+      const outputAuthor = (document.getElementById("output-author").value =
+        book.author);
+      const outputPages = (document.getElementById("output-pages").value =
+        book.pages);
+      const outputPublished = (document.getElementById(
+        "output-published"
+      ).value = book.published);
+      const outputRead = (document.getElementById("output-read").value =
+        book.read);
+    });
+    //Create title element based off books title
+    const bookTitle = document.createElement("p");
+    bookTitle.textContent = `"${book.title}"`;
+    bookTitle.classList.add("book-title");
+    //Assigns bookTitle as Child to bookButton
+    bookButton.appendChild(bookTitle);
+
+    bookContainer.appendChild(bookButton);
   });
-
-  //Create title element based off books title
-  const bookTitle = document.createElement("p");
-  bookTitle.textContent = `"${book.title}"`;
-  bookTitle.classList.add("book-title");
-  //Assigns bookTitle as Child to bookButton
-  bookButton.appendChild(bookTitle);
-
-  bookContainer.appendChild(bookButton);
 });
-
-function addBookToLibrary() {}
